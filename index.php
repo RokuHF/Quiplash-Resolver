d81ce428c1137a8c6e6e96197b8b0370c7925d6b
RokuHF <eric404dev@gmail.com>
2015-07-12 03:48:26 +0200

    index.php
    Input form for resolver.

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<title>Quiplash Resolver.</title>";
+echo "Welcome to the one and only Quiplash resolver!";
+echo "<br> <form action='resolve.php' method='post'> <p>Room code: <input type='text' name='room' /></p> <p><input type='submit' /></p> </form>";
+?>
